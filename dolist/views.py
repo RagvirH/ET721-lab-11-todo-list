36c38399c18756ed0501a10008d9446635ebcbb8
RagvirH <157735804+RagvirH@users.noreply.github.com>
2024-11-01 17:10:25 +0000

    updated

--- dolist/views.py
+++ dolist/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
-
+from . models import ToDolist
 # Create your views here.
 def index(request):
+    todo_items = ToDolist.objects.order_by('id')
+    context = {'todo_items' : todo_items}
     return render(request, 'dolist/index.html')
